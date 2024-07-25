3e3a280b5bf2e412e76e418569d624c12022b93a
Usman-837 <usmantufail837@gmail.com>
2024-07-25 12:58:11 +0500

    add react js functional component folder include slides and code file

---
+++ 5 - React JS Functional Components/my-react-app/src/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const User = () => {
+    return(
+        <>
+        <h1>Hi Functional Component</h1>
+        </>
+    );
+};
+// export User;
+
+// export default User;
