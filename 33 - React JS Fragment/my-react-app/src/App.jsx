d19fbf57e27caecb14e1e36a9535481cb3a6a6f1
Usman-837 <usmantufail837@gmail.com>
2024-08-16 10:42:25 +0500

    add react js fragment folder include slides and code files

---
+++ 33 - React JS Fragment/my-react-app/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Cols from "./Cols";
+
+const App = () => {
+  return(
+    <div>
+      <h1>React Fragment</h1>
+      <table>
+        <tr>
+          <Cols/>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default App;
---
+++ 33 - React JS Fragment/my-react-app/src/Cols.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from "react";
+
+const Cols = () => {
+    return(
+        // <div>
+        // <React.Fragment>
+        // <Fragment>
+        <>
+            <td>Usman</td>
+            <td>Tufail</td>
+        </>
+        // </Fragment>
+        // </React.Fragment>
+        // </div>
+    );
+};
+
+export default Cols;
