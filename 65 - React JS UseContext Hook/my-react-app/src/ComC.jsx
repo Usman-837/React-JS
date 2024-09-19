d4b2d04d6c1b7e7c3c0646a845bb70fe39f72253
Usman-837 <usmantufail837@gmail.com>
2024-09-19 16:36:45 +0500

    add react js useContext Hook folder include slides and code files

---
+++ 65 - React JS UseContext Hook/my-react-app/src/ComC.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from "react";
+import {FirstName , LastName} from "./App";
+
+const ComC = () => {
+
+    const fname = useContext(FirstName);
+    const lname = useContext(LastName);
+    
+    return (
+        <>
+            <h1> My name is {fname} {lname} </h1>
+        </>
+    );
+};
+
+export default ComC;
