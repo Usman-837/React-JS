6b2c0717a4637053a9e4121f710aebc5acd3ac2f
Usman-837 <usmantufail837@gmail.com>
2024-09-23 10:58:11 +0500

    add react js redux action folder include slides and code files

---
+++ 72 - React JS Redux Action/redux-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+import HomeContainer from "./containers/HomeContainer"
+
+function App() {
+
+  return (
+    <>
+      <HomeContainer/>
+    </>
+  )
+}
+
+export default App
